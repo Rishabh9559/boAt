67de739aaf1ccf899ac0351b56bfb81fd6f86c25
Rishabh Kushwaha <Rishabhkushwaha9559@gmail.com>
2024-06-24 10:07:17 +0530

    done

---
+++ js/cart.js
@@ -0,0 +1,31 @@
+let counter = 0;
+
+function increment() {
+  counter++;
+}
+
+function decrement() {
+  counter--;
+}
+
+function get() {
+  return counter;
+}
+
+const inc = document.getElementById("increment");
+const input = document.getElementById("input");
+const dec = document.getElementById("decrement");
+
+inc.addEventListener("click", () => {
+  increment();
+  input.value = get();
+});
+
+dec.addEventListener("click", () => {
+  if (input.value > 0) {
+    decrement();
+  }
+  input.value = get();
+});
+
+
